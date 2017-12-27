bd25856f2844ea8e9cdaa704f37f0e4f9b60a795
zlmzju <zlmzju@gmail.com>
2017-12-27 19:29:30 +0800

    add submodule of caffe

--- models/basenet/googlenet/training_script.sh
+++ models/basenet/googlenet/training_script.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-# Name of the job: PBS can read the command after #, which shell ignores them
-#PBS -N PROJECT_NAME
-
-# Job asks for 1 node (computer) and 2 gpu on each of them (total 4 GPUs)
-#PBS -l nodes=1:ppn=1
-
-# Redirect standard error to standard output
-#PBS -j oe
-
-### Output files
-#PBS -o /share/data/joblog/${PBS_JOBID}.OU
-
-#Remap the free gpus to make the IDs always start from 0
-source /share/data/script/util/remap_free_gpus.sh
-
-# Enter the job's working directory.
-[ "$PBS_O_WORKDIR" != "" ] && cd $PBS_O_WORKDIR
-
 # Our own script
 export GLOG_log_dir=./snapshot
 PROJECT_DIR/caffe/build/tools/caffe train -solver solver.prototxt -weights PROJECT_DIR/models/basenet/googlenet/bvlc_googlenet.caffemodel -gpu 0
